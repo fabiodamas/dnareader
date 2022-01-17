fe7265aff63bf7e8e56937581a525e726c6d2df4
Fabio Damas <fabio.damas@gmail.com>
2022-01-17 10:19:05 -0300

    refactoring the main class name of the project

--- src/main/java/br/com/fabio/dnareader/DnaReaderApplication.java
+++ src/main/java/br/com/fabio/dnareader/DnaReaderApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DnareaderApplication {
+public class DnaReaderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DnareaderApplication.class, args);
+		SpringApplication.run(DnaReaderApplication.class, args);
 	}
 
 }
